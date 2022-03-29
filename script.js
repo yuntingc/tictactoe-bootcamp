3892e72a56a4aacf658bf6400bf35d84707329e0
yuntingc <chiayunting@gmail.com>
2022-03-29 21:50:30 +0800

    update checkWin logic with loop

--- script.js
+++ script.js
@@ -98,6 +98,37 @@ const checkWin = (board) => {
 
   // check every position for all possibilities of winning combinations
 
+  for (let i = 0; i < board.length; i += 1) {
+    
+    // for all horizontal rows
+    if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== '') {
+      console.log('horizontal');
+      return true;
+    }
+
+    // for all vertical columns
+    if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== '') {
+      console.log('vertical');
+      return true;
+    }
+
+    // for diagonals from top left to bottom right
+    if (board[i][i] === board[i+1][i+1] && board [i+2][i+2] && board[i][i] !== '') {
+      console.log('i: ', i);
+      console.log('top left diagonal');
+      return true;
+    }
+
+    // for diagonals from top right to bottom left
+    if (board[i][board.length-1] === board[i+1][board.length-2] && board[i+1][board.length-2] === board[board.length-1][i] && board[i][board.length-1] !== '') {
+      console.log('top right diagonal');
+      return true;
+    }
+
+
+  }
+
+  /*
   // for all horizontal rows
   for (let i = 0; i < 3; i += 1) {
     if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== '') {
@@ -119,8 +150,10 @@ const checkWin = (board) => {
     console.log('diagonal');
     return true;
   }
+  */
 
 }
+
 
 // ===== GAME INITIALISATION LOGIC =====
 
@@ -135,3 +168,4 @@ const initGame = () => {
 };
 
 initGame();
+
